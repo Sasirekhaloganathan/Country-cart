38738a8f0e4f100a5de77f38a29d66de2ecfaaaa
Sasirekhaloganathan <sasirekhavl.22cse@kongu.edu>
2024-04-15 19:16:25 +0530

    2

--- client/src/components/Authentication.jsx
+++ client/src/components/Authentication.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React , {Fragment} from "react";
 import '../Style/Auth.css';
+import { FaUser,FaLock } from "react-icons/fa";
 function Authentication(){
     return(
         <>
