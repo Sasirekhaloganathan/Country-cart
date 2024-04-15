a433fd53753f162f5bb735621af61e810c9d6a3a
Sasirekhaloganathan <sasirekhavl.22cse@kongu.edu>
2024-04-15 16:30:23 +0530

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-
+import {BrowserRoutes , Routes} from 'react-router-dom'
 import Content from './components/content'
+import Home from './components/HomePage'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <Content />
-      </div>
+      <BrowserRoutes>
+      <Routes path="/home" element="Home"></Routes>
+      <Routes path="/content" element="Content"></Routes>
+      </BrowserRoutes>
     </>
   )
 }
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,8 +1,11 @@
 import React , {Fragment} from "react";
+import './HomePage.css';
 const HomePage = () =>
 {
     return(
-        <></>
+        <>
+        <div className="container1">Hello</div>
+        </>
     );
 }
 export default App;
