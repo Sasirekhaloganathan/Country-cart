e5c2d27d017105b3e32c41b56d805c88f1207c35
Sasirekhaloganathan <sasirekhavl.22cse@kongu.edu>
2024-04-16 14:45:01 +0530

    1

--- client/src/Farmercomponents/Farmers.jsx
+++ client/src/Farmercomponents/Farmers.jsx
@@ -3,7 +3,11 @@ import '../FarmerStyle/Farmers.css';
 import Navigation from "../Buyercomponents/Navigation";
 const Farmers = () => {
     return(
-        <></>
+        <>
+        <div className="fcont1">
+            <h2>POST</h2>
+        </div>
+        </>
     );
 }
 export default Farmers;
