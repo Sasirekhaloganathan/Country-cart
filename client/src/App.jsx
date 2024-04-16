3b1c90462cffc25c2cd201151f9d139900e403fe
pradeep sakthi <spradeepsakthi2004@gmail.com>
2024-04-16 11:18:48 +0530

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,7 @@ import './Style/App.css'
 import Authentication from './components/Authentication'
 import Contact from './components/contact'
 import Cart from './components/cart'
-<<<<<<< Updated upstream
-=======
-import Register from './components/register'
-import About from './components/About'
->>>>>>> Stashed changes
+import Register from './components/RegisterPage'
 function App() {
   const [count, setCount] = useState(0)
   return (
@@ -22,12 +18,10 @@ function App() {
       <Route path='/content' element={<Content />}></Route>
       <Route path='/contactus' element={<Contact />}></Route>
       <Route path='/cart' element={<Cart />}></Route>
-<<<<<<< Updated upstream
-=======
-      <Route path='/about' element={<About />}></Route>
+
       <Route path='/register' element={<Register />}></Route>
 
->>>>>>> Stashed changes
+
       </Routes>
       </BrowserRouter>
       
--- client/src/components/RegisterPage.jsx
+++ client/src/components/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import '../../App1.css'
+import '../Style/App1.css'
 
 export default function SignUpPage() {
 
