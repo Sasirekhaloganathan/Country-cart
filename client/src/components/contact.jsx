62aa7aa0b2d6abdc6662885d5f7b2b7b9c1f4dcb
Sasirekhaloganathan <sasirekhavl.22cse@kongu.edu>
2024-04-16 10:42:50 +0530

    1

--- client/src/components/contact.jsx
+++ client/src/components/contact.jsx
@@ -13,7 +13,7 @@ export default function ContactUs() {
      
         <Row className="mb-5 mt-3">
           <Col lg="8">
-            <h1 className="display-4 mb-4">Contact Me</h1>
+            <h1 className="container1">Contact Me</h1>
             <hr className="t_border my-4 ml-0 text-left" />
           </Col>
         </Row>
