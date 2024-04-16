1c59d20cfd3e4a935e9721329d20cdf39737bcec
pradeep sakthi <spradeepsakthi2004@gmail.com>
2024-04-16 13:47:53 +0530

    login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import Home from './components/HomePage'
 import './Style/App.css'
 import Cart from './components/cart'
 import Landing from './components/LandingPage'
+import Login from './components/LoginPage'
+import Register from './components/RegisterPage'
+import ForgotPass from './components/ForgetPasswordPage'
 function App() {
   const [count, setCount] = useState(0)
   return (
@@ -12,6 +15,10 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Landing />}></Route>      
+        <Route path='/home' element={<Home />}></Route>      
+        <Route path='/login' element={<Login />}></Route>      
+        <Route path='/register' element={<Register />}></Route>      
+        <Route path='/forgot' element={<ForgotPass />}></Route>      
       <Route path='/content' element={<Content />}></Route>
       <Route path='/cart' element={<Cart />}></Route>
       <Route path="/cart/:data" component={<Cart />} />
