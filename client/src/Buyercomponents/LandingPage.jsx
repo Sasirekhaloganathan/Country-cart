5c996fd8b5a04dc995dd4ffe343e2e3fd04f6a91
Sasirekhaloganathan <sasirekhavl.22cse@kongu.edu>
2024-04-17 04:29:34 +0530

    1

--- client/src/Buyercomponents/LandingPage.jsx
+++ client/src/Buyercomponents/LandingPage.jsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
                 <div className="logo"><img src="Country_Cart_Logo.png" className="logoicon"></img></div>
 <div className="login">
             <div className="buttons text-center">
-            <span id="userspan">BUYER</span><br></br>
+            <div id="userspan">BUYER</div><br></br>
                 <Link to="/login">
                     <button className="primary-button" id="login_btn"><span>log in</span></button>
                 </Link>
@@ -19,7 +19,7 @@ export default function LandingPage() {
                 </Link>
             </div>
             <div className="buttons text-center">
-            <span id="farmerspan">VENDOR</span><br></br>
+            <div id="farmerspan">VENDOR</div><br></br>
                 <Link to="/login">
                     <button className="primary-button" id="login_btn"><span>log in</span></button>
                 </Link>
