e30339796ff9f46655ac58eb1e411c9c0796d444
pradeep sakthi <spradeepsakthi2004@gmail.com>
2024-04-16 12:01:28 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,7 @@ import {BrowserRouter , Routes, Route} from 'react-router-dom'
 import Content from './components/content'
 import Home from './components/HomePage'
 import './Style/App.css'
-import Contact from './components/contact'
 import Cart from './components/cart'
-import Register from './components/RegisterPage'
 function App() {
   const [count, setCount] = useState(0)
   return (
@@ -14,9 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>      
       <Route path='/content' element={<Content />}></Route>
-      <Route path='/contactus' element={<Contact />}></Route>
       <Route path='/cart' element={<Cart />}></Route>
-      <Route path='/register' element={<Register />}></Route>
+      
 
 
       </Routes>
