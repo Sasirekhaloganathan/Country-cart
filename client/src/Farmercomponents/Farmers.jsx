805ba302b6eafb28a813ab17c1aabe016811c9a1
Sasirekhaloganathan <sasirekhavl.22cse@kongu.edu>
2024-04-16 14:58:08 +0530

    3

--- client/src/Farmercomponents/Farmers.jsx
+++ client/src/Farmercomponents/Farmers.jsx
@@ -5,7 +5,18 @@ const Farmers = () => {
     return(
         <>
         <div className="fcont1">
-            <h2>POST</h2>
+            <h2 className="title" ><b>POST</b></h2>
+            <form action="/action_page.php">
+  <label for="cat">Choose category : </label>
+  <select name="cat" id="cat">
+    <option value="Vegetables">Vegetables</option>
+    <option value="Fruits">Fruits</option>
+    <option value="Greens">Greens</option>
+    <option value="Nuts">Nuts</option>
+  </select>
+  <br></br>
+  <p>Details</p>
+</form>
         </div>
         </>
     );
