818d92d638bce0e896d087b18fd47a62c765bbbc
pradeep sakthi <spradeepsakthi2004@gmail.com>
2024-04-17 10:28:48 +0530

    product

--- server/models/ProductModel.js
+++ server/models/ProductModel.js
@@ -2,8 +2,10 @@ const mongoose=require('mongoose')
 
 const ProductSchema=new mongoose.Schema({
     ProductName:{type: String, required: true},
+    ProductDescription:{tpe:String},
     ProductPrice:{type: Number, required:true , unique: true},
     Quantity: { type: Number, required: true },
+    Image:{type : String}
     
 },
 
