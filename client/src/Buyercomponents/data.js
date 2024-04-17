f0002e1a0860485ff6efd39506855612afe5f301
pradeep sakthi <spradeepsakthi2004@gmail.com>
2024-04-17 06:40:40 +0530

    Update data.js

--- client/src/Buyercomponents/data.js
+++ client/src/Buyercomponents/data.js
@@ -1,12 +1,79 @@
 import img1 from '/icons.png'
-
+import img2 from ''
 const products=[
 
     {
         "id":"1",
-        "name":"product1",
+        "name":"",
+        "price":"Rs.20 / kg",
         "image":img1,
-    }
+    },
+    {
+        "id":"2",
+        "name":"",
+        "price":"Rs.20 / kg",
+        "image":img2,
+    },
+    {
+        "id":"3",
+        "name":"product1",
+        "price":"Rs.20 / kg",
+        "image":img3,
+    },
+    {
+        "id":"4",
+        "name":"product1",
+        "price":"Rs.20 / kg",
+        "image":img4,
+    },
+    {
+        "id":"5",
+        "name":"product1",
+        "price":"Rs.20 / kg",
+        "image":img5,
+    },
+    {
+        "id":"6",
+        "name":"product1",
+        "price":"Rs.20 / kg",
+        "image":img6,
+    },
+    {
+        "id":"7",
+        "name":"product1",
+        "price":"Rs.20 / kg",
+        "image":img7,
+    },
+    {
+        "id":"8",
+        "name":"product1",
+        "price":"Rs.20 / kg",
+        "image":img8,
+    },
+    {
+        "id":"9",
+        "name":"product1",
+        "price":"Rs.20 / kg",
+        "image":img9,
+    },
+    {
+        "id":"10",
+        "name":"product1",
+        "price":"Rs.20 / kg",
+        "image":img10,
+    },
+    {
+        "id":"11",
+        "name":"product1",
+        "price":"Rs.20 / kg",
+        "image":img11,
+    },
+    {
+        "id":"12",
+        "name":"product1",
+        "price":"Rs.20 / kg",
+        "image":img12,
+    },
 ];
 
 export default products;
